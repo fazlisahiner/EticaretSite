f90077cf48f40842efbb948aece72f784f98d5e6
fazlı şahiner <fazlisahiner55@gmail.com>
2024-05-10 12:03:17 +0300

    Add project files.

---
+++ Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace EticaretSite.Models
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+    }
+}
